7f85a69edac0b1e7421d4617881f7b663adbfa41
Sheinehah <maui.sheinehah@gmail.com>
2018-01-24 11:25:12 -1000

    Mofications des routes

--- routes/web.php
+++ routes/web.php
@@ -14,7 +14,16 @@
 Route::get('/', function () {
     return view('welcome');
 });
-
+            // Routes for Authentification
 Auth::routes();
-
+        
 Route::get('/home', 'HomeController@index')->name('home');
+
+            //Routes for links_entity
+Route::get('/links', 'LinkController@index')->name('links');
+Route::get('/links/new', 'LinkController@new')->name('newLink');
+Route::get('/links', 'LinkController@create')->name('createLink');
+Route::get('/links/{linkId}', 'LinkController@show')->name('link');
+Route::get('/links/{linkId}/edit', 'LinkController@edit')->name('editLink');
+Route::get('/links/{linkId}', 'LinkController@update');
+Route::get('/links/{linkId}', 'LinkController@destroy');
