736236bfebc2f4ff8e38697d07be2e5659042654
Sheinehah <maui.sheinehah@gmail.com>
2018-01-24 10:52:25 -1000

    Modification du Seeder et du Factory

--- database/seeds/LinksTableSeeder.php
+++ database/seeds/LinksTableSeeder.php
@@ -13,27 +13,6 @@ class LinksTableSeeder extends Seeder
      */
     public function run()
     {
-        $links = [];
-
-        for($i = 0; $i < 10; $i++):
-            $url = str_random(8);
-            $title = str_random(12); 
-            $user_id = rand(1, 15);
-            $picture = str_random(8); 
-            $category_id = rand(1,3);
-
-            $links[] = [
-                'url'=>$url,
-                'title'=>$title,
-                'user_id'=>$user_id,
-                'picture'=>$picture,
-                'category_id'=>$category_id
-            ];
-            
-        endfor;
-
-        foreach($links AS $link):
-            Link::create($link);
-        endforeach;
+        factory(Link::class, 20)->create();
     }
 }
