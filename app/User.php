4e953cded8ffa2d34efe9e8922c474d1ea12c69d
Sheinehah <maui.sheinehah@gmail.com>
2018-01-24 21:05:02 -1000

    Modif Authentification

--- app/User.php
+++ app/User.php
@@ -15,7 +15,7 @@ class User extends Authenticatable
      * @var array
      */
     protected $fillable = [
-        'name', 'lastname', 'firsname', 'user_type_id', 'email', 'password'
+        'lastname', 'firsname', 'user_type_id', 'email', 'password'
     ];
 
     /**
