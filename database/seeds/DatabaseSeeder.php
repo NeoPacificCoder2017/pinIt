5edf0d87641d8887dc9288b7ffaf96d65920e1ee
Sheinehah <maui.sheinehah@gmail.com>
2018-01-24 17:13:57 -1000

    Modification DatabaseSeeder

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -12,5 +12,7 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         // $this->call(UsersTableSeeder::class);
+        $this->call(CategoriesTableSeeder::class);
+        $this->command->info("Categories Seeder GOOD");
     }
 }
