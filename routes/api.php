58c9020bcb37b4bb4a234bdc1305a7848e05a46a
skat987 <ctuhiti@gmail.com>
2018-02-08 11:53:17 -1000

    friends_entity initial commit

--- routes/api.php
+++ routes/api.php
@@ -16,3 +16,6 @@ use Illuminate\Http\Request;
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+//---------------------- friends_entity ------------------------//
+Route::apiResource('friends', 'FriendController');
--- vendor/composer/autoload_static.php
+++ vendor/composer/autoload_static.php
@@ -320,6 +320,7 @@ class ComposerStaticInit82074a12c02823db41b65438be339ceb
         'App\\Category' => __DIR__ . '/../..' . '/app/Category.php',
         'App\\Console\\Kernel' => __DIR__ . '/../..' . '/app/Console/Kernel.php',
         'App\\Exceptions\\Handler' => __DIR__ . '/../..' . '/app/Exceptions/Handler.php',
+        'App\\Friend' => __DIR__ . '/../..' . '/app/Friend.php',
         'App\\Http\\Controllers\\Auth\\ForgotPasswordController' => __DIR__ . '/../..' . '/app/Http/Controllers/Auth/ForgotPasswordController.php',
         'App\\Http\\Controllers\\Auth\\LoginController' => __DIR__ . '/../..' . '/app/Http/Controllers/Auth/LoginController.php',
         'App\\Http\\Controllers\\Auth\\RegisterController' => __DIR__ . '/../..' . '/app/Http/Controllers/Auth/RegisterController.php',
